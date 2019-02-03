6e5915254d7af5e1597167cd9f084732cbc9c2c4
NevMem <memlolkek@gmail.com>
2019-02-03 19:02:55 +0300

    Critical updates on FullDescription pages(but it's still not working)

--- app/src/main/java/com/nevmem/moneysaver/FullDescriptionActivity.kt
+++ app/src/main/java/com/nevmem/moneysaver/FullDescriptionActivity.kt
@@ -1,7 +1,11 @@
 package com.nevmem.moneysaver
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.SharedElementCallback
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import android.os.PersistableBundle
@@ -16,7 +20,8 @@ import android.view.View
 import kotlinx.android.synthetic.main.full_description.*
 import android.support.v4.view.ViewPropertyAnimatorListenerAdapter
 import android.support.v4.view.ViewCompat
-
+import android.util.AttributeSet
+import android.widget.NumberPicker
 
 
 class FullDescriptionActivity: FragmentActivity() {
@@ -26,10 +31,13 @@ class FullDescriptionActivity: FragmentActivity() {
 
     private var index: Int = 0
     private lateinit var app: App
+    private lateinit var viewModel: FullDescriptionActivityViewModel
 
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.full_description)
+        viewModel = ViewModelProviders.of(this).get(FullDescriptionActivityViewModel::class.java)
+
         window.statusBarColor = ContextCompat.getColor(this, R.color.backgroundColor)
 
         i("description", "Hello from on create method")
@@ -40,15 +48,228 @@ class FullDescriptionActivity: FragmentActivity() {
         recordValueField.text = app.records[index].value.toString()
 
         window.sharedElementEnterTransition.duration = 200
-        tag.text = app.records[index].tags[0]
-        wallet.text = app.records[index].wallet
+
+        viewModel.currentYear.observe(this, Observer {
+            year.text = it
+            if (viewModel.prevYear.value != it) {
+                year.setTextColor(ContextCompat.getColor(this, R.color.specialColor))
+            } else {
+                year.setTextColor(ContextCompat.getColor(this, R.color.mainFontColor))
+            }
+            comparePrevCurrent()
+        })
+        viewModel.currentMonth.observe(this, Observer {
+            month.text = it
+            if (viewModel.prevMonth.value != it) {
+                month.setTextColor(ContextCompat.getColor(this, R.color.specialColor))
+            } else {
+                month.setTextColor(ContextCompat.getColor(this, R.color.mainFontColor))
+            }
+            comparePrevCurrent()
+        })
+        viewModel.currentDay.observe(this, Observer {
+            day.text = it
+            if (viewModel.prevDay.value != it) {
+                day.setTextColor(ContextCompat.getColor(this, R.color.specialColor))
+            } else {
+                day.setTextColor(ContextCompat.getColor(this, R.color.mainFontColor))
+            }
+            comparePrevCurrent()
+        })
+        viewModel.currentHour.observe(this, Observer {
+            hour.text = it
+            if (viewModel.prevHour.value != it) {
+                hour.setTextColor(ContextCompat.getColor(this, R.color.specialColor))
+            } else {
+                hour.setTextColor(ContextCompat.getColor(this, R.color.mainFontColor))
+            }
+            comparePrevCurrent()
+        })
+        viewModel.currentMinute.observe(this, Observer {
+            minute.text = it
+            if (viewModel.prevMinute.value != it) {
+                minute.setTextColor(ContextCompat.getColor(this, R.color.specialColor))
+            } else {
+                minute.setTextColor(ContextCompat.getColor(this, R.color.mainFontColor))
+            }
+            comparePrevCurrent()
+        })
+
+        viewModel.needChange.observe(this, Observer {
+            if (it!!) {
+                saveChangesButton.visibility = View.VISIBLE
+            } else {
+                saveChangesButton.visibility = View.GONE
+            }
+        })
+
+        yearWrapper.setOnClickListener {
+            val builder = AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+            val picker = NumberPicker(this)
+            picker.maxValue = 2020
+            picker.minValue = 2018
+            picker.value = viewModel.currentYear.value!!.toInt()
+            builder.setTitle("Choose year")
+                .setMessage("Please enter new year for this record")
+                .setPositiveButton(android.R.string.yes) { _, which -> run {
+                        viewModel.currentYear.value = picker.value.toString()
+                    } }
+                .setNegativeButton(android.R.string.no) { _, which -> run {
+                    System.out.println(which)
+                } }
+                .setView(picker)
+                .show()
+        }
+
+        monthWrapper.setOnClickListener {
+            val builder = AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+            val picker = NumberPicker(this)
+            picker.minValue = 0
+            picker.maxValue = 11
+            picker.displayedValues = arrayOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+            for (index in 0 until(picker.displayedValues.size))
+                if (picker.displayedValues[index] == viewModel.currentMonth.value)
+                    picker.value = index
+            builder.setTitle("Choose month")
+                .setMessage("Please choose new month for this record")
+                .setPositiveButton(android.R.string.yes) { _, which -> run {
+                    val newMonth = picker.displayedValues[picker.value]
+                    viewModel.currentMonth.value = newMonth
+                }}
+                .setView(picker)
+                .show()
+        }
+
+        dayWrapper.setOnClickListener {
+            val builder = AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+            val picker = NumberPicker(this)
+            picker.minValue = 1
+            picker.maxValue = amountOfDaysInMonth(viewModel.currentYear.value!!.toInt(), viewModel.currentMonth.value.toString())
+            picker.value = viewModel.currentDay.value!!.toInt()
+            builder.setTitle("Choose day of month")
+                .setMessage("Please choose new day for this record")
+                .setPositiveButton(android.R.string.yes) { _, which -> run {
+                    viewModel.currentDay.value = fillToFormat(picker.value.toString())
+                }}
+                .setView(picker)
+                .show()
+        }
+
+        hourWrapper.setOnClickListener {
+            val builder = AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+            val picker = NumberPicker(this)
+            picker.minValue = 0
+            picker.maxValue = 23
+            picker.value = viewModel.currentHour.value!!.toInt()
+            builder.setTitle("Choose hour of a day")
+                .setMessage("Please choose new hour for this record")
+                .setPositiveButton(android.R.string.yes) { _, which -> run {
+                    viewModel.currentHour.value = fillToFormat(picker.value.toString())
+                }}
+                .setView(picker)
+                .show()
+        }
+
+        minuteWrapper.setOnClickListener {
+            val builder = AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+            val picker = NumberPicker(this)
+            picker.minValue = 0
+            picker.maxValue = 59
+            picker.value = viewModel.currentMinute.value!!.toInt()
+            builder.setTitle("Choose minute of a hour")
+                .setMessage("Please choose new minute for this record")
+                .setPositiveButton(android.R.string.yes) { _, which -> run {
+                    viewModel.currentMinute.value = fillToFormat(picker.value.toString())
+                }}
+                .setView(picker)
+                .show()
+        }
+
+        fillValues()
+
         Handler().postDelayed({
             fadeInValues()
         }, 400)
     }
 
+    private fun fillToFormat(current: String): String {
+        var res = current
+        while (res.length < 2)
+            res = "0$res"
+        return current
+    }
+
+    private fun comparePrevCurrent() {
+        var diffCount = 0
+        if (viewModel.currentYear.value != viewModel.prevYear.value) diffCount++
+        if (viewModel.currentMonth.value != viewModel.prevMonth.value) diffCount++
+        if (viewModel.currentDay.value != viewModel.prevDay.value) diffCount++
+        if (viewModel.currentHour.value != viewModel.prevHour.value) diffCount++
+        if (viewModel.currentMinute.value != viewModel.prevMinute.value) diffCount++
+        viewModel.needChange.value = (diffCount != 0)
+    }
+
+    private fun isLeap(year: Int): Boolean {
+        return (year % 4) == 0 && (year % 100 != 0 || year % 400 == 0)
+    }
+
+    private fun amountOfDaysInMonth(year: Int, month: String): Int {
+        var leap = 0
+        if (isLeap(year))
+            leap = 1
+        when (month) {
+            "January", "March", "May", "July", "August", "October", "December" -> return 31
+            "February" -> return 28 + leap
+        }
+        return 30
+    }
+
+    private fun getMonth(index: Int): String {
+        when(index - 1) {
+            0->return "January"
+            1->return "February"
+            2->return "March"
+            3->return "April"
+            4->return "May"
+            5->return "June"
+            6->return "July"
+            7->return "August"
+            8->return "September"
+            9->return "October"
+            10->return "November"
+            11->return "December"
+        }
+        return "Unknown"
+    }
+
+    fun fillValues() {
+        tag.text = app.records[index].tags[0]
+        wallet.text = app.records[index].wallet
+
+        viewModel.currentYear.value = app.records[index].date.year.toString()
+        viewModel.prevYear.value = app.records[index].date.year.toString()
+
+        viewModel.currentMonth.value = getMonth(app.records[index].date.month)
+        viewModel.prevMonth.value = getMonth(app.records[index].date.month)
+
+        viewModel.currentDay.value = fillToFormat(app.records[index].date.day.toString())
+        viewModel.prevDay.value = fillToFormat(app.records[index].date.day.toString())
+
+        viewModel.currentHour.value = fillToFormat(app.records[index].date.hour.toString())
+        viewModel.prevHour.value = fillToFormat(app.records[index].date.hour.toString())
+
+        viewModel.currentMinute.value = fillToFormat(app.records[index].date.minute.toString())
+        viewModel.prevMinute.value = fillToFormat(app.records[index].date.minute.toString())
+    }
+
     fun fadeInValues() {
         tag.animate().alpha(1f).setDuration(200).start()
         wallet.animate().alpha(1f).setDuration(200).start()
+        yearWrapper.animate().alpha(1f).setDuration(200).start()
+        monthWrapper.animate().alpha(1f).setDuration(200).start()
+        dayWrapper.animate().alpha(1f).setDuration(200).start()
+        hourWrapper.animate().alpha(1f).setDuration(200).start()
+        minuteWrapper.animate().alpha(1f).setDuration(200).start()
+        info_text_1.animate().alpha(1f).setDuration(200).start()
     }
 }
---
+++ app/src/main/java/com/nevmem/moneysaver/FullDescriptionActivityViewModel.kt
@@ -0,0 +1,27 @@
+package com.nevmem.moneysaver
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class FullDescriptionActivityViewModel : ViewModel() {
+    var prevMonth = MutableLiveData<String>()
+    var currentMonth = MutableLiveData<String>()
+
+    var prevYear = MutableLiveData<String>()
+    var currentYear = MutableLiveData<String>()
+
+    var prevDay = MutableLiveData<String>()
+    var currentDay = MutableLiveData<String>()
+
+    var prevHour = MutableLiveData<String>()
+    var currentHour = MutableLiveData<String>()
+
+    var prevMinute= MutableLiveData<String>()
+    var currentMinute = MutableLiveData<String>()
+
+    var needChange = MutableLiveData<Boolean>()
+
+    init {
+        needChange.value = false
+    }
+}
