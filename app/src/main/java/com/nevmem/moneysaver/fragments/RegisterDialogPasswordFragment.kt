ba8f25650b13e55088befe80848388ebbf9a5aa1
NevMem <memlolkek@gmail.com>
2019-04-19 00:10:45 +0300

    Added some simple conditions for password choosing and confirming fragment

--- app/src/main/java/com/nevmem/moneysaver/fragments/RegisterDialogPasswordFragment.kt
+++ app/src/main/java/com/nevmem/moneysaver/fragments/RegisterDialogPasswordFragment.kt
@@ -6,9 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nevmem.moneysaver.R
+import kotlinx.android.synthetic.main.register_dialog_password.*
 
 class RegisterDialogPasswordFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.register_dialog_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        passwordField.changeHandler = {
+            when {
+                it.length >= 6 -> {
+                    passwordField.success = true
+                    passwordField.error = ""
+                }
+                it.isNotEmpty() -> {
+                    passwordField.success = false
+                    passwordField.error = "Password is too short"
+                }
+                else -> {
+                    passwordField.success = false
+                    passwordField.error = ""
+                }
+            }
+        }
+        confirmPasswordField.changeHandler = {
+            if (it == passwordField.text && it.length >= 6) {
+                confirmPasswordField.success = true
+                confirmPasswordField.error = ""
+            } else if (it.isNotEmpty()) {
+                confirmPasswordField.error = "Passwords are not equal"
+                confirmPasswordField.success = false
+            } else {
+                confirmPasswordField.error = ""
+                confirmPasswordField.success = false
+            }
+        }
     }
 }
