cb98f6f5f41fdd0b1b34d27028c8837364a43dcc
NevMem <memlolkek@gmail.com>
2019-05-09 13:19:46 +0300

    Removed unused imports

--- app/src/main/java/com/nevmem/moneysaver/fragments/DashboardPageMonthDescriptionFragment.kt
+++ app/src/main/java/com/nevmem/moneysaver/fragments/DashboardPageMonthDescriptionFragment.kt
@@ -3,7 +3,6 @@ package com.nevmem.moneysaver.fragments
 import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,7 +11,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.nevmem.moneysaver.App
 import com.nevmem.moneysaver.R
