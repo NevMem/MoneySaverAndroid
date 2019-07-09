0d3058a2b3a3d28da9379c9539a4db9507cb444a
NevMem <memlolkek@gmail.com>
2019-07-09 15:17:09 +0300

    Moved view creating into special function

--- app/src/main/java/com/nevmem/moneysaver/views/ConfirmationDialog.kt
+++ app/src/main/java/com/nevmem/moneysaver/views/ConfirmationDialog.kt
@@ -3,6 +3,7 @@ package com.nevmem.moneysaver.views
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatDialogFragment
 import com.nevmem.moneysaver.R
 import kotlinx.android.synthetic.main.confirmation_dialog_popup.view.*
@@ -11,11 +12,14 @@ class ConfirmationDialog(private var header: String, private var message: String
     private var okCallback: (() -> Unit)? = null
     private var dismissCallback: (() -> Unit)? = null
 
+    private fun createView(): View {
+        val inflater = activity?.layoutInflater ?: throw IllegalStateException("Layout inflater cannot be null")
+        return inflater.inflate(R.layout.confirmation_dialog_popup, null)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity, R.style.CustomDialogStyle)
-        val inflater = activity?.layoutInflater
-            ?: throw IllegalStateException("Something went wrong, layout inflater have to be not null")
-        val view = inflater.inflate(R.layout.confirmation_dialog_popup, null)
+        val view = createView()
         view.popupMessage.text = message
         return builder.setView(view)
             .setTitle(header)
