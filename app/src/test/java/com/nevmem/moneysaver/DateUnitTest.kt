eb796b796c4bdc7d22ca4d39ecc6671f151e47e3
NevMem <memlolkek@gmail.com>
2019-09-01 16:01:57 +0300

    Added a few tests

---
+++ app/src/test/java/com/nevmem/moneysaver/DateUnitTest.kt
@@ -0,0 +1,41 @@
+package com.nevmem.moneysaver
+
+import com.nevmem.moneysaver.data.RecordDate
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Host tests
+ */
+
+class DateUnitTest {
+    @Test
+    fun checkDateFromJsonParsing() {
+        val expected = RecordDate(2017, 12, 21, 23, 53)
+        val json = JSONObject()
+        json.put("year", expected.year)
+        json.put("month", expected.month)
+        json.put("day", expected.day)
+        json.put("hour", expected.hour)
+        json.put("minute", expected.minute)
+        val found = RecordDate.fromJSON(json)
+        Assert.assertEquals(expected, found)
+    }
+
+    @Test
+    fun checkDateFromJsonParsingEmptyJson() {
+        val json = JSONObject()
+        val found = RecordDate.fromJSON(json)
+        Assert.assertEquals(RecordDate(), found)
+    }
+
+    @Test
+    fun checkDateFromJsonParsingNotOnlyInts() {
+        val json = JSONObject()
+        json.put("year", "some year")
+        json.put("month", "1,3")
+        val found = RecordDate.fromJSON(json)
+        Assert.assertEquals(RecordDate(), found)
+    }
+}
---
+++ app/src/test/java/com/nevmem/moneysaver/HistoryRepositoryTests.kt
@@ -0,0 +1,71 @@
+package com.nevmem.moneysaver
+
+import com.nevmem.moneysaver.data.util.HistoryRepositoryParsers
+import com.nevmem.moneysaver.data.util.ParseError
+import com.nevmem.moneysaver.data.util.ParsedValue
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+
+class HistoryRepositoryTests {
+    @Test
+    fun historyRepositoryParserBasicTest() {
+        val json = JSONObject()
+        val parsed = HistoryRepositoryParsers.parseServerLoadedResponse(json)
+        if (parsed is ParseError) {
+            Assert.assertEquals(Vars.unknownFormat, parsed.reason)
+        } else {
+            Assert.fail("Should return an error")
+        }
+    }
+
+    @Test
+    fun historyRepositoryParserServerErrorTest() {
+        val error = "Small server problems"
+        val json = JSONObject("{type: 'error', error: '$error'}")
+        val parsed = HistoryRepositoryParsers.parseServerLoadedResponse(json)
+        if (parsed is ParseError) {
+            Assert.assertEquals(error, parsed.reason)
+        } else {
+            Assert.fail()
+        }
+    }
+
+    @Test
+    fun historyRepositoryParserNoDataTest() {
+        val json = JSONObject("{type: 'ok'}")
+        val parsed = HistoryRepositoryParsers.parseServerLoadedResponse(json)
+        if (parsed is ParseError) {
+            Assert.assertEquals(Vars.unspecifiedData, parsed.reason)
+        } else {
+            Assert.fail()
+        }
+    }
+
+    @Test
+    fun historyRepositoryParserEmptyDataTest() {
+        val json = JSONObject("{type: 'ok', data: []}")
+        val parsed = HistoryRepositoryParsers.parseServerLoadedResponse(json)
+        if (parsed is ParseError) {
+            Assert.fail("It's not an error")
+        } else if (parsed is ParsedValue<*>) {
+            if (parsed.parsed is ArrayList<*>) {
+                val buffer = parsed.parsed as ArrayList<*>
+                Assert.assertEquals(0, buffer.size)
+            } else {
+                Assert.fail("Parsed value is not an ArrayList")
+            }
+        }
+    }
+
+    @Test
+    fun historyRepositoryParserRecordIsNotAnObjectTest() {
+        val json = JSONObject("{type: 'ok', data: [[],{}]}")
+        val parsed = HistoryRepositoryParsers.parseServerLoadedResponse(json)
+        if (parsed is ParseError) {
+            Assert.assertEquals(Vars.corruptedRecord, parsed.reason)
+        } else if (parsed is ParsedValue<*>) {
+            Assert.fail()
+        }
+    }
+}
